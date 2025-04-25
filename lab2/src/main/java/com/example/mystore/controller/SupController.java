dd21bb17175af2e96319e48a3c455168319d1ad4
Ahsaber <ah_saber@163.com>
2025-04-25 18:19:15 +0800

    finish 2.1.1

--- lab2/src/main/java/com/example/mystore/controller/SupController.java
+++ lab2/src/main/java/com/example/mystore/controller/SupController.java
@@ -2,6 +2,7 @@ package com.example.mystore.controller;
 
 import com.example.mystore.core.Result;
 import com.example.mystore.entity.Supplier;
+import com.example.mystore.repository.SupRepository;
 import com.example.mystore.service.SupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,10 +10,30 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller//要访问到index页就必须使用这个注解(与@GetMapping("/index")配合）而不能用@RestController，否则与GetMapping返回的就是"index"字符而非index页面
-public class SupController {
-    @Autowired
-    private SupService supService;
+public class SupController implements SupService {
 
+    @Autowired
+    private SupRepository supRepository;
+
+    @Override
+    public Result checkSupplier(Supplier supplier) {
+        Supplier supObj = supRepository.findSupplier(supplier);
+        Result result = new Result();
+        if (supObj == null) {
+            result.setCode(404);
+            result.setMsg("用户不存在！");
+        } else {
+            if (!supplier.getSpassword().equals(supObj.getSpassword())) {
+                result.setCode(403);
+                result.setMsg("密码错！");
+            } else {
+                result.setCode(200);
+                result.setMsg("验证通过");
+                result.setData(supObj);
+            }
+        }
+        return result;
+    }
     @GetMapping("/index")
     public String init(Model model) {
         Result result = new Result();
@@ -26,7 +47,7 @@ public class SupController {
     public String loginCheck(Model model, Supplier supplier) {
         model.addAttribute("scode", supplier.getScode());
         model.addAttribute("spassword", supplier.getSpassword());
-        Result result = supService.checkSupplier(supplier);
+        Result result = checkSupplier(supplier);
         model.addAttribute("result", result);
         if (result.getCode() == 200) {
             Supplier supObj = (Supplier) result.getData();
@@ -40,6 +61,6 @@ public class SupController {
     @RequestMapping("/login")//运行时访问http://localhost:8080/login?scode=SXLC001A&&spassword=888
     @ResponseBody//默认返回到URL,只有使用这个注解才表示返回JSON
     public Result loginCheck(Supplier supplier) {
-        return supService.checkSupplier(supplier);
+        return checkSupplier(supplier);
     }
 }
